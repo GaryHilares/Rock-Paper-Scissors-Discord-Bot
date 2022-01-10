cf660589180ae2598b90da9dcfb027a2ffc26ecf
GaryNLOL <dev.garynlol@gmail.com>
2022-01-10 14:53:58 -0500

    Corrected code according to linting

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
-from bot.bot import discord_client
 import sys
+from bot.bot import discord_client
 
 def main():
     token = ''
     if len(sys.argv) < 2:
         print('Please add the token file path.')
         return
-    with open(sys.argv[1]) as ifile:
+    with open(sys.argv[1], encoding='UTF-8') as ifile:
         token = ifile.read()
     discord_client.run(token)
 
